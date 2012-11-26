aab13af6269218ac09ccebf7291024ad5009dbc8
Christopher G Dolan <cdolan@mit.edu>
2012-11-25 22:49:44 -0500

    added template_module

---
+++ template_module.py
@@ -0,0 +1,17 @@
+# Owner: <your name> (<your email>)
+# Summary: <One sentence summary of module functionality>
+# Notes: Input: <what the input is>, Output: <what the output is>, <other notes>
+
+from module import *
+
+class TransposeModule(Module):
+        
+    def generate_output(self):
+        #your code goes here
+
+        #input to module can be accessed by calling self.get_input(<input port>)        
+
+        #Once processing is complete, set the output(s) of the module by calling self.set_output(<output>, <output port>)
+
+    
+        
