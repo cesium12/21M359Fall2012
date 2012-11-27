4eaa5a6b75d0be05043dfa0311d844e257c43a02
Alan Huang <cesium@mit.edu>
2012-11-27 15:55:44 -0500

    Multiple transpose (requested by Peter in class)

--- transpose_by_n.py
+++ transpose_by_n.py
@@ -6,23 +6,13 @@ from module import *
 from chord import *
 
 class TransposeByN(Module):
-        
     def generate_output(self):
-        
-        steps=self.get_input("in1")
-        assert isinstance(steps,int)
         chords = self.get_input("in0")
-        for chord in chords:
-            assert isinstance(chord,Chord)
+        steps = self.get_input("in1")
+        assert len(chords) == len(steps)
         output = []
-        for chord in chords:
-            output.append(chord.transpose(steps))
-        self.set_output(output,"out0")
-        #your code goes here
-
-        #input to module can be accessed by calling self.get_input(<input port>)        
-
-        #Once processing is complete, set the output(s) of the module by calling self.set_output(<output>, <output port>)
-
-    
-        
+        for chord, step in zip(chords, steps):
+            assert isinstance(chord, Chord)
+            assert isinstance(step, int)
+            output.append(chord + step)
+        self.set_output(output, "out0")
