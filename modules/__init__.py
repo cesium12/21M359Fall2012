c99d345b8401bffd5a449768c7e0b55a9994f9cc
Alan Huang <cesium@mit.edu>
2012-11-29 15:55:32 -0500

    Namespaces!

--- modules/__init__.py
+++ modules/__init__.py
@@ -4,9 +4,9 @@
 # to make it available in system.py: 'from <module_file_name_without_.py> import *'
 
 from chord import *
-from start_module import *
-from end_module import *
-from transpose_up_one_module import *
-from base_at_zero_module import *
-from leftward_pack_module import *
-from transpose_by_n_module import *
+from start import *
+from end import *
+from transpose_up_one import *
+from base_at_zero import *
+from leftward_pack import *
+from transpose_by_n import *
