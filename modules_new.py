08a80c1cf43c377123fed6d4fd268fbc3bf6b4b5
Charles Epstein <charles.epstein@gmail.com>
2012-11-29 14:36:27 -0500

    Revert "Revert "Send chords to reduced form""
    This reverts commit 9cd93f20b9d2733cf76cd2df5ad7bb1116511245.

--- modules_new.py
+++ modules_new.py
@@ -3,8 +3,12 @@
 # Notes: Once your module is defined, add the following line
 # to make it available in system.py: 'from <module_file_name_without_.py> import *'
 
-from chord import *
+from chord_mod import *
 from start_module import *
 from end_module import *
 from transpose_up_one_module import *
+<<<<<<< HEAD
 from transpose_by_n import *
+=======
+from reduce_module import *
+>>>>>>> Send chords to reduced form
